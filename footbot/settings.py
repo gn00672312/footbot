d11675c861172cdf322a3b677adda160494a5de2
roywu <roywu@cloudybay.com.tw>
2018-01-19 10:33:01 +0800

    modify settings

--- footbot/settings.py
+++ footbot/settings.py
@@ -79,7 +79,12 @@ WSGI_APPLICATION = 'footbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+    },
+}
+
 
 TEST_RUNNER = 'footbot.testing.DatabaselessTestRunner'
 
