725184ba98801cfc5466897e5321edff6ac93ea8
roywu <roywu@cloudybay.com.tw>
2018-01-19 15:16:32 +0800

    modify yaml

--- footbot/settings.py
+++ footbot/settings.py
@@ -81,8 +81,9 @@ WSGI_APPLICATION = 'footbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
